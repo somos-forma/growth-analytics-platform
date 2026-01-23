8a844d68e9e7a5b52d32b018d009f698a68adaf9
DavidMarioLC <davidmariolc.dev@gmail.com>
2026-01-22 21:16:09 -0500

    feat: add GET endpoint for fetching tables with Google Auth

---
+++ app/api/analytics/tables/route.ts
@@ -0,0 +1,25 @@
+import { GoogleAuth } from "google-auth-library";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const TARGET_AUDIENCE = process.env.CLOUD_RUN_URL ?? "https://example.com";
+  try {
+    const auth = new GoogleAuth({
+      credentials: {
+        client_email: process.env.GCP_CLIENT_EMAIL,
+        private_key: process.env.GCP_PRIVATE_KEY,
+      },
+    });
+    const client = await auth.getIdTokenClient(TARGET_AUDIENCE);
+
+    const response = await client.request({
+      url: `${TARGET_AUDIENCE}/tables`,
+      method: "GET",
+    });
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Failed to fetch ID token" }, { status: 500 });
+  }
+}
